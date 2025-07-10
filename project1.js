c1311dabee3d0724dddcc88bfcad3406f9591f2f
SpanduXD <107503855+SpanduXD@users.noreply.github.com>
2025-07-10 22:27:28 +0530

    Initial commit

---
+++ project1.js
@@ -0,0 +1,18 @@
+alert("Hello Myself HeartBeat and I am new to Web Developing hope u like my website");
+alert("Can you try my calculator plssss :)");
+let a=prompt("Enter the first Number");
+let b=prompt("Enter the second number");
+let c=prompt("Enter the operations you want(+,-,*,/)");
+// if(c=='+'){
+//     let result=a+b;
+// }
+// else if(c=='-'){
+//     let result=a-b;
+// }
+// else if(c=='*'){
+//     let result=a*b;
+// }
+// else if(c=='/'){
+//     let result=a/b;
+// }
+alert(`The result is ${eval(`${a} ${c} ${b}`)}`);
